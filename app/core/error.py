f9c77c17538d712bb0184a2a72b87963fd0f5e5a
Antox-22 <grasso.antonino09@gmail.com>
2026-05-20 02:05:02 +0200

    Reinvented Error Module

--- app/core/error.py
+++ app/core/error.py
@@ -1,7 +1,7 @@
 from app.core.log import log
-import functools
+import functools, json
 
-def safe(message=None, retry=False):
+def safe(message=None, retry=False, window=None, data={}, exception_data: dict={}):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
@@ -21,6 +21,25 @@ def safe(message=None, retry=False):
                     # ERROR: Log the exception with the function name
                     log.exception(f"Errore in {func.__name__}")
 
+                    if not window:
+                        log.debug("Window not taken.")
+                        return message
+
+                    if e in exception_data:
+                        error_msg = exception_data[e]
+                        safe_data = json.dumps(data)
+                        showError(window, error_msg, safe_data)
+                    else:
+                        safe_error_data = json.dumps({"error": str(e)})
+                        showError(window, "unexpected_error", safe_error_data)
+
                 return message
         return wrapper
     return decorator
+
+def showError(window, message, data):
+    if not window:
+        log.exception(f"Unable to comunicato w/ frontend")
+        return
+    window.resize(800, 600)
+    window.evaluate_js(f"window.showError('{message}', {data})")
